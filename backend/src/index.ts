f570715a825dbce2986b6cf9d685a6efb6cc3f17
EnjoyKojima <majiko2627kojima@gmail.com>
2024-07-11 23:51:06 +0900

    tRPC以外はインストール済み

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,27 @@
 import { Elysia } from "elysia";
+import { bearer } from '@elysiajs/bearer'
+import { cron } from '@elysiajs/cron'
+import { jwt } from '@elysiajs/jwt'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia()
+  .use(bearer())
+  .use(jwt({
+    name: 'jwt',
+    secret: 'Fischl von Luftschloss Narfidort'
+  }))
+  .use(cron({
+    name: 'heartbeat',
+    pattern: '*/10 * * * * *',
+    run() {
+      console.log('Heartbeat')
+    }
+  }))
+
+app.get("/", () => "Hello Elysia")
+app.get("/auth", () => "Hello Elysia")
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
